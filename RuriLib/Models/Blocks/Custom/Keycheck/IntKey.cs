d5ae6e36773dc3ad85834015c7f846a33fdea15e
maher ali <109253719+abosalim10@users.noreply.github.com>
2022-07-14 05:56:08 +0300

    Initial commit

---
+++ RuriLib/Models/Blocks/Custom/Keycheck/IntKey.cs
@@ -0,0 +1,17 @@
+﻿using RuriLib.Models.Blocks.Settings;
+using RuriLib.Models.Conditions.Comparisons;
+
+namespace RuriLib.Models.Blocks.Custom.Keycheck
+{
+    public class IntKey : Key
+    {
+        public NumComparison Comparison { get; set; } = NumComparison.EqualTo;
+
+        public IntKey()
+        {
+            Left = new BlockSetting() { InputMode = SettingInputMode.Variable, FixedSetting = new IntSetting() };
+            Left.InputVariableName = "data.RESPONSECODE";
+            Right = new BlockSetting() { FixedSetting = new IntSetting() };
+        }
+    }
+}
