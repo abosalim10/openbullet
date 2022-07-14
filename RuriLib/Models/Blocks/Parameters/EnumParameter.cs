c234061282c1ac8797fdc63e4d94db37c94d51bc
maher ali <109253719+abosalim10@users.noreply.github.com>
2022-07-14 05:56:14 +0300

    Initialize net6.0

---
+++ RuriLib/Models/Blocks/Parameters/EnumParameter.cs
@@ -0,0 +1,27 @@
+﻿using RuriLib.Models.Blocks.Settings;
+using System;
+
+namespace RuriLib.Models.Blocks.Parameters
+{
+    public class EnumParameter : BlockParameter
+    {
+        public Type EnumType { get; set; }
+        public string DefaultValue { get; set; }
+
+        public EnumParameter()
+        {
+
+        }
+
+        public EnumParameter(string name, Type enumType, string defaultValue)
+        {
+            Name = name;
+            EnumType = enumType;
+            DefaultValue = defaultValue;
+        }
+
+        public override BlockSetting ToBlockSetting()
+            => new BlockSetting { Name = Name, FixedSetting = new EnumSetting { EnumType = EnumType, Value = DefaultValue },
+                InputMode = InputMode };
+    }
+}
---
+++ RuriLib/Models/Proxies/ProxySource/FileProxySource.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuriLib.Models.Proxies.ProxySources
+{
+    public class FileProxySource : ProxySource
+    {
+        public string FileName { get; set; } = string.Empty;
+
+        public FileProxySource(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public override async Task<IEnumerable<Proxy>> GetAll()
+        {
+            var lines = await File.ReadAllLinesAsync(FileName);
+
+            return lines
+                .Select(l => Proxy.TryParse(l, out var proxy, DefaultType, DefaultUsername, DefaultPassword) ? proxy : null)
+                .Where(p => p != null);
+        }
+    }
+}
