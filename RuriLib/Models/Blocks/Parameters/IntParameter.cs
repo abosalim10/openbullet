d059cef944c6b49675b8196678446bc4768b241e
maher ali <109253719+abosalim10@users.noreply.github.com>
2022-07-14 05:56:17 +0300

    Initialize debug-comp

---
+++ RuriLib/Models/Blocks/Parameters/IntParameter.cs
@@ -0,0 +1,31 @@
+﻿using RuriLib.Models.Blocks.Settings;
+
+namespace RuriLib.Models.Blocks.Parameters
+{
+    public class IntParameter : BlockParameter
+    {
+        public int DefaultValue { get; set; }
+
+        public IntParameter()
+        {
+
+        }
+
+        public IntParameter(string name, int defaultValue = 0)
+        {
+            Name = name;
+            DefaultValue = defaultValue;
+        }
+
+        public IntParameter(string name, string defaultVariableName = "")
+        {
+            Name = name;
+            DefaultVariableName = defaultVariableName;
+            InputMode = SettingInputMode.Variable;
+        }
+
+        public override BlockSetting ToBlockSetting()
+            => new BlockSetting { Name = Name, FixedSetting = new IntSetting { Value = DefaultValue },
+                InputMode = InputMode };
+    }
+}
