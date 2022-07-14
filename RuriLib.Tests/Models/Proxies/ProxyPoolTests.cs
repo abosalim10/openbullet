3f3b8c4ea9ac1f379079b1ce6c903c8ecfe1d339
maher ali <109253719+abosalim10@users.noreply.github.com>
2022-07-14 05:56:28 +0300

    Initialize dependabot/nuget/Updater.Native/Newtonsoft.Json-13.0.1

---
+++ RuriLib.Tests/Models/Proxies/ProxyPoolTests.cs
@@ -0,0 +1,82 @@
+﻿using RuriLib.Models.Proxies;
+using RuriLib.Models.Proxies.ProxySources;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RuriLib.Tests.Models.Proxies
+{
+    public class ProxyPoolTests
+    {
+        [Fact]
+        public async Task RemoveDuplicates_ListWithDuplicates_ReturnDistinct()
+        {
+            ListProxySource source = new(new Proxy[]
+            {
+                new Proxy("127.0.0.1", 8000),
+                new Proxy("127.0.0.1", 8000)
+            });
+
+            var pool = new ProxyPool(new ProxySource[] { source });
+
+            await pool.ReloadAll();
+            pool.RemoveDuplicates();
+            Assert.Single(pool.Proxies);
+        }
+
+        [Fact]
+        public async Task GetProxy_Available_ReturnValidProxy()
+        {
+            ListProxySource source = new(new Proxy[]
+            {
+                new Proxy("127.0.0.1", 8000)
+            });
+
+            var pool = new ProxyPool(new ProxySource[] { source });
+
+            await pool.ReloadAll();
+            Assert.NotNull(pool.GetProxy());
+        }
+
+        [Fact]
+        public async Task GetProxy_AllBusy_ReturnNull()
+        {
+            ListProxySource source = new(new Proxy[]
+            {
+                new Proxy("127.0.0.1", 8000) { ProxyStatus = ProxyStatus.Busy }
+            });
+
+            var pool = new ProxyPool(new ProxySource[] { source });
+
+            await pool.ReloadAll();
+            Assert.Null(pool.GetProxy());
+        }
+
+        [Fact]
+        public async Task GetProxy_EvenBusy_ReturnValidProxy()
+        {
+            ListProxySource source = new(new Proxy[]
+            {
+                new Proxy("127.0.0.1", 8000) { ProxyStatus = ProxyStatus.Busy }
+            });
+
+            var pool = new ProxyPool(new ProxySource[] { source });
+
+            await pool.ReloadAll();
+            Assert.NotNull(pool.GetProxy(true));
+        }
+
+        [Fact]
+        public async Task GetProxy_MaxUses_ReturnNull()
+        {
+            ListProxySource source = new(new Proxy[]
+            {
+                new Proxy("127.0.0.1", 8000) { TotalUses = 3 }
+            });
+
+            var pool = new ProxyPool(new ProxySource[] { source });
+
+            await pool.ReloadAll();
+            Assert.Null(pool.GetProxy(true, 3));
+        }
+    }
+}
---
+++ RuriLib/Helpers/RootChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace RuriLib.Helpers
+{
+    public static class RootChecker
+    {
+        [DllImport("libc")]
+        private static extern uint geteuid();
+
+        public static bool IsRoot()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                bool isAdmin;
+                using (var identity = WindowsIdentity.GetCurrent())
+                {
+                    var principal = new WindowsPrincipal(identity);
+                    isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+                }
+
+                return isAdmin;
+            }
+            else
+            {
+                return geteuid() == 0;
+            }
+        }
+
+        public static bool IsUnixRoot()
+            => !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && geteuid() == 0;
+    }
+}
